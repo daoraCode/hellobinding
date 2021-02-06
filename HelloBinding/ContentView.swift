884985676241c1a4357106842f081c48e7330643
daoraCode <severin.mboukou@@gmail.com>
2021-02-06 19:52:00 +0100

    Initial Commit

---
+++ HelloBinding/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  HelloBinding
+//
+//  Created by Antony Mboukou on 06/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isOn: Bool = false
+    
+    var body: some View {
+    
+        VStack {
+            
+         LightBulb(isOn: $isOn)
+            
+        }
+        
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .background(isOn ? Color.white : Color.black)
+        .edgesIgnoringSafeArea(.all)
+        
+        
+        }
+        
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+struct LightBulb : View {
+    
+    
+    @Binding var isOn: Bool
+    
+    var body: some View {
+        
+        Group {
+            
+            if isOn {
+                
+                Image(systemName: "lightbulb")
+                    .font(.largeTitle)
+                
+            } else {
+                
+                Image(systemName: "lightbulb.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.yellow)
+            }
+            
+           
+        }.onTapGesture {
+            isOn.toggle()
+        }
+    }
+}
